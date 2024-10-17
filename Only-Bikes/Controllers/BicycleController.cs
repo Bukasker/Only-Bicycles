2dc49cece9a37bedcaca07d2c626e13fea67ec05
Bukasker <kamil.buk.01@gmail.com>
2024-10-17 23:50:47 +0200

    Add Authorize attribute to all controllers.

--- Only-Bikes/Controllers/BicycleController.cs
+++ Only-Bikes/Controllers/BicycleController.cs
@@ -1,37 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Only_bicycles.Models;
 using Only_bicycles.ViewModels;
 
-namespace Only_bicycles.Controllers
+namespace Only_Bikes.Controllers;
+
+public class BicycleController(
+    IBicycleRepository bicycleRepository,
+    ICategoryRepository category,
+    IGenderCategoryRepository genderCategory)
+    : Controller
 {
-	public class BicycleController : Controller
-	{
-		private readonly IBicycleRepository _bicycleRepository;
-		private readonly ICategoryRepository _categoryRepository;
-		private readonly IGenderCategoryRepository _genderRepository;
-		
-		public BicycleController(IBicycleRepository bicycleRepository, ICategoryRepository category, IGenderCategoryRepository genderCategory)
-		{
-			_bicycleRepository = bicycleRepository;
-			_categoryRepository = category;
-			_genderRepository = genderCategory;
-		}
+    [Authorize]
+    public IActionResult List()
+    {
+        //ViewBag.CurrentCategory = "Rower Górski";
+        //return View(_bicycleRepository.AllBicycle);
+        BicycleListViewModel bicycleListViewModel = new BicycleListViewModel(bicycleRepository.AllBicycle, "All Bikes",
+            genderCategory.AllGenderCategories);
+        return View(bicycleListViewModel);
+    }
 
-		public IActionResult List() 
-		{
-			//ViewBag.CurrentCategory = "Rower Górski";
-			//return View(_bicycleRepository.AllBicycle);
-			BicycleListViewModel bicycleListViewModel = new BicycleListViewModel(_bicycleRepository.AllBicycle,"All Bikes",_genderRepository.AllGenderCategories);
-			return View(bicycleListViewModel);
-		}
-
-		public IActionResult Details(int id)
-		{
-			var bicycle = _bicycleRepository.GetBicycleId(id);
-			if (bicycle == null)
-				return NotFound();
-			return View(bicycle);
-		}
-
-	}
+    [Authorize]
+    public IActionResult Details(int id)
+    {
+        var bicycle = bicycleRepository.GetBicycleId(id);
+        if (bicycle == null)
+            return NotFound();
+        return View(bicycle);
+    }
 }
--- Only-Bikes/Controllers/ShoppingCartController.cs
+++ Only-Bikes/Controllers/ShoppingCartController.cs
@@ -1,98 +1,93 @@
-﻿using Only_bicycles.Models;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Only_Bikes.Models;
+using Only_bicycles.Models;
 using Only_bicycles.ViewModels;
+using Only_Bikes.Models;
 using Only_Bikes.Services;
 
-namespace Only_bicycles.Controllers
+namespace Only_Bikes.Controllers;
+
+public class ShoppingCartController(
+    IBicycleRepository bicycleRepository,
+    IShoppingCart shoppingCart,
+    OnlyBicycleDbContext context)
+    : Controller
 {
-    public class ShoppingCartController : Controller
+    [Authorize]
+    public ViewResult Index()
     {
-        private readonly IBicycleRepository _bicycleRepository;
-        private readonly IShoppingCart _shoppingCart;
-        private readonly OnlyBicycleDbContext _context;
+        var items = shoppingCart.GetShoppingCartItems();
+        shoppingCart.ShoppingCartItems = items;
 
-        public ShoppingCartController(
-            IBicycleRepository bicycleRepository,
-            IShoppingCart shoppingCart,
-            OnlyBicycleDbContext context
-            )
-        {
-            _bicycleRepository = bicycleRepository;
-            _shoppingCart = shoppingCart;
-            _context = context;
-        }
-        public ViewResult Index()
-        {
-            var items = _shoppingCart.GetShoppingCartItems();
-            _shoppingCart.ShoppingCartItems = items;
+        var shoppingCartViewModel = new ShoppingCartViewModel(shoppingCart, shoppingCart.GetShoppingCartTotal());
+        shoppingCartViewModel.TotalCost = ReservationService.GetTotalCost(shoppingCartViewModel.EndDate, shoppingCart);
+        return View(shoppingCartViewModel);
+    }
 
-            var shoppingCartViewModel = new ShoppingCartViewModel(_shoppingCart, _shoppingCart.GetShoppingCartTotal());
-            shoppingCartViewModel.TotalCost = ReservationService.GetTotalCost(shoppingCartViewModel.EndDate, _shoppingCart);
-            return View(shoppingCartViewModel);
+    public RedirectToActionResult AddToShoppingCart(int bicycleId)
+    {
+        var selectedBike = bicycleRepository.AllBicycle.FirstOrDefault(p => p.BicycleId == bicycleId);
+
+        if (selectedBike != null)
+        {
+            shoppingCart.AddToCart(selectedBike);
         }
 
-        public RedirectToActionResult AddToShoppingCart(int bicycleId)
-        {
-            var selectedBike = _bicycleRepository.AllBicycle.FirstOrDefault(p => p.BicycleId == bicycleId);
+        return RedirectToAction("Index");
+    }
 
-            if (selectedBike != null)
+    [Authorize]
+    [HttpPost]
+    public IActionResult UpdateTotalCost(ShoppingCartViewModel model)
+    {
+        model.TotalCost = ReservationService.GetTotalCost(model.EndDate, shoppingCart);
+        return View("Index", model);
+    }
+
+
+    public RedirectToActionResult RemoveFromShoppingCart(int bicycleId)
+    {
+        var selectedBike = bicycleRepository.AllBicycle.FirstOrDefault(p => p.BicycleId == bicycleId);
+
+        if (selectedBike != null)
+        {
+            shoppingCart.RemoveFromCart(selectedBike);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult CreateReservation(ShoppingCartViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            List<Reservation> reservations = new();
+            foreach (var bicycle in model.ShoppingCart.GetShoppingCartItems())
             {
-                _shoppingCart.AddToCart(selectedBike);
-            }
-            return RedirectToAction("Index");
-        }
-
-		[HttpPost]
-		public IActionResult UpdateTotalCost(ShoppingCartViewModel model)
-        {
-			model.TotalCost = ReservationService.GetTotalCost(model.EndDate, _shoppingCart);
-			return View("Index", model);
-		}
-
-
-		public RedirectToActionResult RemoveFromShoppingCart(int bicycleId)
-        {
-            var selectedBike = _bicycleRepository.AllBicycle.FirstOrDefault(p => p.BicycleId == bicycleId);
-
-            if (selectedBike != null)
-            {
-                _shoppingCart.RemoveFromCart(selectedBike);
-            }
-            return RedirectToAction("Index");
-        }
-
-        [HttpPost]
-        public IActionResult CreateReservation(ShoppingCartViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                List<Reservation> reservations = new();
-                foreach(var bicycle in model.ShoppingCart.GetShoppingCartItems())
+                var reservation = new Reservation
                 {
-                    var reservation = new Reservation
-                    {
-                        EndDate = model.EndDate,
-                        FirstName = model.FirstName,
-                        LastName = model.LastName,
-                        PhoneNumber = model.PhoneNumber,
-                        BicycleId = bicycle.BicycleId,
-                    };
-                    reservations.Add(reservation);
-                }
-
-                _context.Reservations.AddRange(reservations);
-                _context.SaveChanges();
-
-                return RedirectToAction("Summary");
+                    EndDate = model.EndDate,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    PhoneNumber = model.PhoneNumber,
+                    BicycleId = bicycle.BicycleId,
+                };
+                reservations.Add(reservation);
             }
 
-            return View("Index", model);
+            context.Reservations.AddRange(reservations);
+            context.SaveChanges();
+
+            return RedirectToAction("Summary");
         }
 
-        public IActionResult Summary()
-        {
-            return View();
-        }
+        return View("Index", model);
+    }
+
+    public IActionResult Summary()
+    {
+        return View();
     }
 }
