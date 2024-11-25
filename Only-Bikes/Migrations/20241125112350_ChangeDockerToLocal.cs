673b34fc0a0e474a5bb9b04b817e0d2278c9b176
Bukasker <kamil.buk.01@gmail.com>
2024-11-25 12:56:55 +0100

    Changed appsettings to run project on local host

---
+++ Only-Bikes/Migrations/20241125112350_ChangeDockerToLocal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Only_Bikes.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDockerToLocal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("2301399d-20f9-43cb-8bb4-0dab870bd13a"),
+                columns: new[] { "PasswordHash", "PasswordSalt" },
+                values: new object[] { "1aiziBybGL6HHb0Lait3WdoQUQ7JeIlg1QB+rPd3ZBU=", "CNrViJrDAPgoWmRRehDRjQ==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("2301399d-20f9-43cb-8bb4-0dab870bd13a"),
+                columns: new[] { "PasswordHash", "PasswordSalt" },
+                values: new object[] { "LVdI6+y0J6gl07Lm2vi3cO36Tc6VhD7H831E8qXaf38=", "bIszapWIy/cVWG0x/h4yfA==" });
+        }
+    }
+}
--- Only-Bikes/Migrations/20241125115108_ChangeDockerToLocalUpdate.Designer.cs
+++ Only-Bikes/Migrations/20241125115108_ChangeDockerToLocalUpdate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Only_Bikes.Models;
 
@@ -11,9 +12,11 @@ using Only_Bikes.Models;
 namespace Only_Bikes.Migrations
 {
     [DbContext(typeof(OnlyBicycleDbContext))]
-    partial class OnlyBicycleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241125115108_ChangeDockerToLocalUpdate")]
+    partial class ChangeDockerToLocalUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
